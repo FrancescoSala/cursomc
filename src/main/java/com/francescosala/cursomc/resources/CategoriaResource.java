c90ca660c9c534df22422db5fe730a16a42befeb
Francesco Salamone <fransalamone@gmail.com>
2025-02-17 16:56:06 -0300

    Testing the first domain class - Category

--- src/main/java/com/francescosala/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/francescosala/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,12 @@
 package com.francescosala.cursomc.resources;
 
+import com.francescosala.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,10 +14,16 @@ public class CategoriaResource {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest test";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
-
-
 
 }
