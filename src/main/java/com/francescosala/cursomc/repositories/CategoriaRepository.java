ffe67be7ab058e6e16a50512c82ad05565ea8322
Francesco Salamone <fransalamone@gmail.com>
2025-02-18 17:44:06 -0300

    Created repository and service for Category + H2 Database

---
+++ src/main/java/com/francescosala/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,12 @@
+package com.francescosala.cursomc.repositories;
+
+import com.francescosala.cursomc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+
+
+}
---
+++ src/main/java/com/francescosala/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.francescosala.cursomc.services;
+
+import com.francescosala.cursomc.domain.Categoria;
+import com.francescosala.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria find(Integer id) {
+
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+
+}
