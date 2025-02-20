39b4fbcacbea77dc89b05818d664380947f053f5
Francesco Salamone <fransalamone@gmail.com>
2025-02-20 09:51:27 -0300

    Endpoint /clientes/{id}

--- src/main/java/com/francescosala/cursomc/domain/Endereco.java
+++ src/main/java/com/francescosala/cursomc/domain/Endereco.java
@@ -1,5 +1,6 @@
 package com.francescosala.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -21,6 +22,7 @@ public class Endereco implements Serializable {
     private String bairro;
     private String cep;
 
+    @JsonBackReference
     @ManyToOne
     @JoinColumn(name="cliente_id")
     private Cliente cliente;
--- src/main/java/com/francescosala/cursomc/domain/Estado.java
+++ src/main/java/com/francescosala/cursomc/domain/Estado.java
@@ -1,5 +1,6 @@
 package com.francescosala.cursomc.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -19,6 +20,7 @@ public class Estado implements Serializable {
     private Integer id;
     private String nome;
 
+    @JsonBackReference
     @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades = new ArrayList<>();
 
